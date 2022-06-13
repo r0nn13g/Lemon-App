7bdf272b2a245b24fb565b0299d6cce44a84cb2b
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-06-12 21:35:25 -0400

    edited restaurant cards

--- front-end/src/Components/Restaurant.js
+++ front-end/src/Components/Restaurant.js
@@ -6,28 +6,21 @@ function Restaurant( {singleRestaurant} ) {
 
   return (
     <div className='restaurant-container'>
-            <Link className='restLink' to={`/restaurant/${singleRestaurant.id}`}>
-        <div className='restaurant-data'>
-          <div className='image-container'>
-          <img className="restaurant-image"  src={singleRestaurant.restaurant_sprites} alt='restaurant stock'/>
-          </div>
+        <Link className='restLink' to={`/restaurant/${singleRestaurant.id}`}>
+          <div className='restaurant-data'>
+            <div className='image-container'>
+              <img className="restaurant-image"  src={singleRestaurant.restaurant_sprites} alt='restaurant stock'/>
+            </div>
             <div className='restaurant-name'>
-              <div>
-                  <h3>{singleRestaurant.name}</h3>
-              </div>
+                {singleRestaurant.name}
             </div>
             <div className='restaurant-type'>
-                <div>
-                  {singleRestaurant.cuisine_type}
-                </div>
+                <em>{singleRestaurant.cuisine_type}</em>
             </div>
             <div className='restaurant-address'>
-                <div>
-                  {singleRestaurant.add}
-                </div>
+                {singleRestaurant.add}
             </div>
-           
-        </div>
+          </div>
         </Link>
     </div>
   )
