7e9823670302da8933f542ef19aa15f7813a323d
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-06-14 14:59:15 -0400

    added hook for active indext and changing active index

--- front-end/src/Components/AnimatedSidebar.js
+++ front-end/src/Components/AnimatedSidebar.js
@@ -38,6 +38,24 @@ const sidebarNavItems = [
 const AnimatedSidebar = () => {
     const [activeIndex, setActiveIndex] = useState(0);
     const [stepHeight, setStepHeight] = useState(0);
+    const sidebarReg = useRef();
+    const location = useLocation;
+    
+    useEffect(() => {
+      setTimeout(() => {
+        const sidebaritem = sidebarRef.current.querySelector('.sidebar_menu_item');
+        indicatorRef.current.style.height = `${sidebarItem.clientHeight}px`;
+        setStepHeight(sidebarItem.clientHeight);
+      }, 50)
+    }, []);
+
+    // change active index
+    useEffect(() => {
+      const curPath = window.location.pathname.split('/')[1];
+      const activeItem = sidebarNavItems.findIndex(item => item.section === curPath);
+      setActiveIndex(curPath.length === 0 ? 0 : activeItem);
+  }, [location]);
+    
       return (
         <div className="animated-sidebar-container">
 
