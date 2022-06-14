3383e67f9481e89c40696e8403fc3979bd43b037
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-06-14 14:43:00 -0400

    added AnimatedSidebar Component, sidebarNavitems Arr, useState

---
+++ front-end/src/Components/AnimatedSidebar.js
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+import { link, useLocation } from 'react-router-dom';
+
+
+const sidebarNavItems = [
+  {
+      display: 'Dashboard',
+      icon: <i className='bx bx-home'></i>,
+      to: '/',
+      section: ''
+  },
+  {
+      display: 'Getting Started',
+      icon: <i className='bx bx-star'></i>,
+      to: '/started',
+      section: 'started'
+  },
+  {
+      display: 'Calendar',
+      icon: <i className='bx bx-calendar'></i>,
+      to: '/calendar',
+      section: 'calendar'
+  },
+  {
+      display: 'User',
+      icon: <i className='bx bx-user'></i>,
+      to: '/user',
+      section: 'user'
+  },
+  {
+      display: 'Orders',
+      icon: <i className='bx bx-receipt'></i>,
+      to: '/order',
+      section: 'order'
+  },
+]
+
+const AnimatedSidebar = () => {
+      return (
+        <div className="animated-sidebar-container">
+
+        </div>
+      )
+}
+
+export default AnimatedSidebar;
