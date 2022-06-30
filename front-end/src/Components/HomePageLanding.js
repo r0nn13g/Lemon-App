a321c11fdb2ec9cd699a22a6b3cae48bde0ab2da
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-06-29 23:51:58 -0400

    change: added Datacards to homepage

--- front-end/src/Components/HomePageLanding.js
+++ front-end/src/Components/HomePageLanding.js
@@ -2,7 +2,7 @@ import React from 'react';
 import"../Styles/homePageLanding.css";
 import Slideshow from "./Slideshow.js";
 import Carousel from "./CarouselComp.js";
-import sampleImage from "../assets/splashImage1.jpeg"
+import DataCard from './DataCard.js';
 
 let sampleText = "Everyone knows that NYC is the place to go if you want some delicious pizza. I mean they even have their own style of pizza! But, did you know that New York City was the birthplace of pizza in America? Using skills learned from his hometown Naples, Italy, the first pizza in the US was crafted in the heart of Little Italy by Italian baker, Gennaro Lombardi. Fun Fact: Lombardi’s Pizza was established in 1905 and is still around today!"
 
@@ -10,16 +10,17 @@ const HomePageLanding = () => {
   return (
     <div className='home-page-wrapper'>
         <Carousel />
-          <div className='slide-show-wrapper'>
+        <div className='slide-show-wrapper'>
           <Slideshow/>
-          </div>
+        </div>
       <div className="home-main-content-container">
         <p id="main-content-container-p">{sampleText}</p>
           <br/>
-        <img id="home-content-img" src={sampleImage} alt="" />
+          <div className="home-content-image-container">
+          <DataCard />
+          <DataCard />
           <br/>
-        <p id="main-content-container-p">{sampleText}</p>
-          <br/>
+          </div>
       </div>
     </div>
   )
