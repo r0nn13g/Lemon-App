a539652cea74b73aab96578ab1b85daf015580dc
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-06-30 00:24:03 -0400

    change: dataCard styling on mobile and full screen devices

--- front-end/src/Components/HomePageLanding.js
+++ front-end/src/Components/HomePageLanding.js
@@ -4,7 +4,7 @@ import Slideshow from "./Slideshow.js";
 import Carousel from "./CarouselComp.js";
 import DataCard from './DataCard.js';
 
-let sampleText = "Everyone knows that NYC is the place to go if you want some delicious pizza. I mean they even have their own style of pizza! But, did you know that New York City was the birthplace of pizza in America? Using skills learned from his hometown Naples, Italy, the first pizza in the US was crafted in the heart of Little Italy by Italian baker, Gennaro Lombardi. Fun Fact: Lombardi’s Pizza was established in 1905 and is still around today!"
+let sampleText = "Everyone knows that NYC is the place to go if you want some delicious pizza. I mean they even have their own style of pizza! But, did you know that New York City was the birthplace of pizza in America?"
 
 const HomePageLanding = () => {
   return (
