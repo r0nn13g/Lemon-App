10bb8b03da1467e552ae9304209fd7e2eb075273
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-03-31 01:29:13 -0400

    fixed home page css dark mode bugs

--- front-end/src/Pages/HomePage.js
+++ front-end/src/Pages/HomePage.js
@@ -55,7 +55,7 @@ function Home({ loginText }) {
             </div>
           )}
           { isChecked ? (
-            <div>hello</div>
+            <div>Choose a restaurant near you!</div>
           ):(
             false
           )}
