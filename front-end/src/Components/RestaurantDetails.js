1e5e91ed42be9942fe75a52a3e5726ff8b027e39
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-04-03 10:22:53 -0400

    changed: font style

--- front-end/src/Components/RestaurantDetails.js
+++ front-end/src/Components/RestaurantDetails.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import "../Styles/products.css";
 import { useEffect, useState } from 'react';
 import ProductByRestaurant from "./ProductByRestaurant";
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 // import {useParams, useNavigate} from "react-router-dom";
 const API = process.env.REACT_APP_API_URL;
 
 function RestaurantDetails() {
     const [restaurant, setRestaurant ] = useState([]);
     const { id } = useParams();
-    
+    let navigate = useNavigate()
     useEffect(() => {
         axios.get(`${API}/restaurants/${id}`)
             .then((res) => {
@@ -23,13 +23,13 @@ function RestaurantDetails() {
     <div className="restaurant-products-container">
         <div className="restaurant-details">
             <div><h1>{restaurant.name}</h1></div>
-            {/* <div><h5>{restaurant.cuisine_type}</h5></div> */}
+            <div><h5>{restaurant.cuisine_type}</h5></div>
             <div><h5>{restaurant.add}</h5></div>
         </div>
             {<ProductByRestaurant id={id}/>}
-        {/* <div className='button-link'>
+        <div className='button-link'>
                 <button id="back-btn" onClick={() => navigate(-1)}>Go Back</button>
-        </div> */}
+        </div>
     </div>
   )
 };
