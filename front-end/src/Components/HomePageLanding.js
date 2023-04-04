1e2bc8b8ad14e2d426b9a28658480199c5572f47
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-04-03 20:24:30 -0400

    changed: dataCard, carousel and section header color

--- front-end/src/Components/HomePageLanding.js
+++ front-end/src/Components/HomePageLanding.js
@@ -7,7 +7,7 @@ import DataCard3 from './DataCard3.js';
 import DataCard4 from './DataCard4.js';
 import InfoIcons from './InfoIcons.js';
 import Carousel from "./CarouselComp.js";
-import mealkitSplash from '../assets/mealkitsSplash.jpeg';
+// import mealkitSplash from '../assets/mealkitsSplash.jpeg';
 
 let sampleText = "Everyone knows that NYC is the place to go if you want some delicious pizza. I mean they even have their own style of pizza! But, did you know that New York City was the birthplace of pizza in America?"
 
@@ -17,27 +17,29 @@ const HomePageLanding = () => {
           <div className='carousel-wrapper'>
             <Carousel />
           </div>
-            <img id="top-splash-page" src={mealkitSplash} alt="meal-kit" />
+          <div className='info-icons-container'>
             <InfoIcons />
+          </div>
           <div className="home-content-image-container">
             <DataCard />
             <DataCard2 />
           </div>
-      <div className="home-main-content-container">
-        <div className='main-content-container-p'>
-          <section>{sampleText}</section>
-          <section id="section-heading"><h1>"Incredible app with great purpose" - The New York Chimes</h1></section>
-          <section>{sampleText}</section>
-          <section><h1>"Changing the Environment" - The New York Toast</h1></section>
-        </div>
+
+    <div className="home-main-content-container">
+      <div className='main-content-container-p'>
+        <section>{sampleText}</section>
+        <section id="section-heading"><h1>"Incredible app with great purpose" - The New York Chimes</h1></section>
+        <section>{sampleText}</section>
+        <section><h1>"Changing the Environment" - The New York Toast</h1></section>
       </div>
-      <div className="home-content-image-container">
-          <DataCard3 />
-          <DataCard4 />
-          </div>
+    </div>
+    <div className="home-content-image-container">
+      <DataCard3 />
+      <DataCard4 />
+    </div>
       <Map />
     </div>
   )
-}
+};
 
 export default HomePageLanding;
