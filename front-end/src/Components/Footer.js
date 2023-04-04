858c866637de7aa6e426e4529728d3f2006c0e1d
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-04-04 03:30:07 -0400

    fixed: overlapping elements and footer styling

--- front-end/src/Components/Footer.js
+++ front-end/src/Components/Footer.js
@@ -1,24 +1,31 @@
 import "../Styles/footer.css";
 import nycDoe from '../assets/NYC_DOE_Logo.png';
 import nycGov from '../assets/nygov-logo.png';
+
 function Footer() {
   return (
     <div className="footer">
       <div className="nyc-doe-logo">
-        <img src={nycDoe} alt="p"/>
-      <div className="nyc-gov-logo">
-        <img src={nycGov} alt="p"/>
-      </div>
-      </div>
-      <ul>
+          <img src={nycDoe} alt="p"/>
+        <div className="nyc-gov-logo">
+          <img src={nycGov} alt="p"/>
+        </div>
+       </div>
+      <ul id="left-ul">
         <li>Resources</li>
         <li>Snap</li>
         <li>NY.Gov</li>
         <li>apply for benefits</li>
       </ul>
-      <div className="content-container">
-      <p>Lemon®</p>
-      </div>
+      {/* <ul id="right-ul">
+        <li>Resources</li>
+        <li>Snap</li>
+        <li>NY.Gov</li>
+        <li>apply for benefits</li>
+      </ul> */}
+        <div className="content-container">
+        <h4>Lemon®</h4>
+        </div>
       </div>
   )
 }
