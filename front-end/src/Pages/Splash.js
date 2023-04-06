b94b78562d1f1f4b17d2cdded9133646d8bb7bb0
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2023-04-05 20:32:35 -0400

    fixed: carousel on web browser

---
+++ front-end/src/Pages/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Styles/splash.css"
+
+function Splash() {
+  return (
+    <div className='splash-container'>
+      
+    </div>
+  )
+}
+
+export default Splash
